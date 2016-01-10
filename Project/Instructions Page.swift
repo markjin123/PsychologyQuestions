01ed1d6e5e0a380aa2360ac1c636e636939fbaea
JacquelineJia <jackiesisy@gmail.com>
2016-01-09 20:17:25 -0500

    first commit

---
+++ Project/Instructions Page.swift
@@ -0,0 +1,50 @@
+//
+//  Instructions Page.swift
+//  Project
+//
+//  Created by sisi on 2016-01-07.
+//  Copyright © 2016 Jacqueline Jia. All rights reserved.
+//
+
+import UIKit
+
+class Instructions_Page: UIViewController {
+
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    @IBOutlet weak var labelParagraph: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.labelTitle.alpha -= 1
+        self.labelParagraph.alpha -= 1
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animateWithDuration(0.5, delay: 0, options: [], animations: {self.labelTitle.alpha = 1}, completion: nil)
+        UIView.animateWithDuration(0.5, delay: 0.5, options: [], animations: {self.labelParagraph.alpha = 1}, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by sisi on 2016-01-05.
+//  Copyright © 2016 Jacqueline Jia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var background: UIImageView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func startButtonPress(sender: UIButton) {
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.startButton.alpha -= 1
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animateWithDuration(1, delay: 0, options: [], animations: {self.startButton.alpha = 1}, completion: nil)
+    }
+}
+
